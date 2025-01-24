84aece1c10fda12abef954f6043805fcde64121b
WiredMind2 <william@michaud.re>
2025-01-24 19:58:50 +0100

    segfault fix

--- Request.h
+++ Request.h
@@ -15,52 +15,51 @@ La classe Request contient deux surcharges d'opérateurs pour la sérialisation 
 */
 
 struct Infos {
-    Infos() : dateTime(nullptr) {}
-    Infos(const string& ip, const string& userLogName, const string& userName,
-          const string& dateTime, const string& method, const string& protocol, 
-          int statusCode, int size, const string& referer, const string& userAgent)
-        : ip(ip), userLogName(userLogName), userName(userName), 
-          method(method), protocol(protocol), statusCode(statusCode), 
-          size(size), referer(referer), userAgent(userAgent) {
-            this->dateTime = new DateTime(dateTime);
-          }
-    ~Infos() {
-        if (dateTime != nullptr) {
-            delete dateTime;
-            dateTime = nullptr;
-        }
-    }
+	Infos() : dateTime(nullptr) {}
+	Infos(const string& ip, const string& userLogName, const string& userName,
+		  const string& dateTime, const string& method, const string& protocol, 
+		  int statusCode, int size, const string& referer, const string& userAgent)
+		: ip(ip), userLogName(userLogName), userName(userName), 
+		  method(method), protocol(protocol), statusCode(statusCode), 
+		  size(size), referer(referer), userAgent(userAgent) {
+			this->dateTime = new DateTime(dateTime);
+		  }
 
-    string ip;
-    string userLogName;
-    string userName;
-    DateTime *dateTime;
-    string method;
-    string protocol;
-    int statusCode;
-    int size;
-    string referer;
-    string userAgent;
+	string ip;
+	string userLogName;
+	string userName;
+	DateTime *dateTime;
+	string method;
+	string protocol;
+	int statusCode;
+	int size;
+	string referer;
+	string userAgent;
 };
 
 class Request {
 public:
-    Request() {}
-    Request(const string& ip, const string& userLogName, const string& userName, 
-            const string& dateTime, const string& method, const string& resource, 
-            const string& protocol, int statusCode, int size, const string& referer, 
-            const string& userAgent)
-        : resource(resource), infos(ip, userLogName, userName, dateTime, method, protocol, statusCode, size, referer, userAgent) {}
+	Request() {}
+	Request(const string& ip, const string& userLogName, const string& userName, 
+			const string& dateTime, const string& method, const string& resource, 
+			const string& protocol, int statusCode, int size, const string& referer, 
+			const string& userAgent)
+		: resource(resource), infos(ip, userLogName, userName, dateTime, method, protocol, statusCode, size, referer, userAgent) {}
 
-    friend ostream& operator<<(ostream& os, const Request& abonne);
-    friend istream& operator>>(istream& is, Request& abonne);
+	~Request() {
+		cerr << "Deleting request" << endl;
+		delete infos.dateTime;
+	}
 
-    friend class Graph;
-    friend class Infos;
+	friend ostream& operator<<(ostream& os, const Request& abonne);
+	friend istream& operator>>(istream& is, Request& abonne);
+
+	friend class Graph;
+	friend class Infos;
 
 private:
-    string resource;
-    Infos infos;
+	string resource;
+	Infos infos;
 };
 
 #endif // REQUEST_H
